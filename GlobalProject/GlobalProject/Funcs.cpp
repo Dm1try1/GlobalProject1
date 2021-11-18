3ea6c9ddc8ee30278287bb410c94971fd52425b7
Vitaliy Kuksin <kuksin_2001@mail.ru>
2021-11-18 23:06:34 +0300

    Реализация функции вычитания

--- GlobalProject/GlobalProject/Funcs.cpp
+++ GlobalProject/GlobalProject/Funcs.cpp
@@ -2,4 +2,4 @@
 double MySum(double b, double b1)
 {
 	return b + b1;
-}
+}double MyVich(double c, double c1){	return c - c1; }
--- GlobalProject/GlobalProject/Funcs.h
+++ GlobalProject/GlobalProject/Funcs.h
@@ -2,4 +2,5 @@
 #ifndef __FUNCS_H__ 
 #define __FUNCS_H__ 
 double MySum(double, double);
+double MyVich(double, double);
 #endif // __FUNCS_H__
